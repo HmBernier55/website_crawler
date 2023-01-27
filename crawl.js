5f479a6235847e6bfc14b0b0e9ff837b8e65952c
Hunter Bernier <hunter.bernier@duke.edu>
2023-01-27 13:27:09 -0600

    added a getURLsFromHTML function and tests for this function

--- crawl.js
+++ crawl.js
@@ -1,3 +1,31 @@
+const {JSDOM} = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === '/') {
+            // relative
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`error with relative url: ${err.message}`)
+            }
+        } else {
+            //absolute
+            try {
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`error with absolute url: ${err.message}`)
+            }
+        }
+    }
+    return urls
+}
+
 function normalizeURL(url) {
     const myURL = new URL(url)
     let fullPath = `${myURL.host}${myURL.pathname}`
@@ -8,5 +36,6 @@ function normalizeURL(url) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
